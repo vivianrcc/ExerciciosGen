f97b3c423b134fb74ed37dcda2ef9d10a487d5b0
vivianrcc <vivianrosanacc@gmail.com>
2024-11-04 13:09:28 -0300

    add new exe

--- condicionais/src/condicionais/Exe05Switch.java
+++ condicionais/src/condicionais/Exe05Switch.java
@@ -1,3 +1,7 @@
+/*Escreva um algoritmo em Java que leia o código de um item 
+ *(número inteiro de 1 a 6) e a quantidade comprada deste item (número inteiro).
+ *A seguir, mostre na tela o valor total da conta e o nome do produto que foi comprado.
+ */
 package condicionais;
 
 import java.util.Scanner;
