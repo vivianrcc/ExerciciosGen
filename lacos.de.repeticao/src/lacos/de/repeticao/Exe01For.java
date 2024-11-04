9a97eb8deb27c286a1b89951493813276d211c89
vivianrcc <vivianrosanacc@gmail.com>
2024-11-04 16:01:19 -0300

    add new exercise for

--- lacos.de.repeticao/src/lacos/de/repeticao/Exe01For.java
+++ lacos.de.repeticao/src/lacos/de/repeticao/Exe01For.java
@@ -10,36 +10,36 @@ package lacos.de.repeticao;
 import java.util.Scanner;
 
 public class Exe01For {
-    
-    public static void main(String[] args) {
-        
-        Scanner scanner = new Scanner(System.in);
-        int num1, num2;
-        
-        // ENTRADA DE DADOS
-        System.out.print("Digite o número 1: ");
-        num1 = scanner.nextInt();
-        
-        System.out.print("Digite o número 2: ");
-        num2 = scanner.nextInt();
-        
-        // VERIFICANDO SE NUM1 E MENOR QUE NUM2
-        if (num1 > num2) {
-            System.out.println("Intervalo inválido! O primeiro número deve ser menor que o segundo.");
-            scanner.close();
-            return; 
-        }
 
-        System.out.println("Números múltiplos de 3 e 5 entre " + num1 + " e " + num2 + ":");
-        
-        // INICIO DO LOO´P FOR 
-        for (int i = num1; i <= num2; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println(i);
-            }
-        }
-        
-        scanner.close();
-    }
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int num1, num2;
+
+		// ENTRADA DE DADOS
+		System.out.print("Digite o número 1: ");
+		num1 = scanner.nextInt();
+
+		System.out.print("Digite o número 2: ");
+		num2 = scanner.nextInt();
+
+		// VERIFICANDO SE NUM1 E MENOR QUE NUM2
+		if (num1 > num2) {
+			System.out.println("ERRO! O primeiro número deve ser menor que o segundo.");
+			scanner.close();
+			return;
+		}
+
+		System.out.println("Números múltiplos de 3 e 5 entre " + num1 + " e " + num2 + ":");
+
+		// INICIO DO LOO´P FOR
+		for (int i = num1; i <= num2; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				System.out.println(i);
+			}
+		}
+
+		scanner.close();
+	}
 }
-	
+//CATCODE
